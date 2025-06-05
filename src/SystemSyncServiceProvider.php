0816edd4f04ddc62eb3e3037a8ef69d10fe2c470
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2025-06-05 15:21:18 +0600

    system-sync-update

--- src/SystemSyncServiceProvider.php
+++ src/SystemSyncServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Doc\SystemSync;
 
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,24 +11,26 @@ class SystemSyncServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if (php_sapi_name() !== 'cli') {
-            try {
-                $url = request()->getHost();
-                $response = Http::post(base64_decode('aHR0cHM6Ly9hcGkuZG9jLmxhcmF2ZWwuYmFuZ2xhZGVzaGlzb2Z0d2FyZS5jb20vYXBpL3ZlcmlmeS9kb21haW4='), [
-                    'domain' => $url,
-                ]);
+        if (File::exists(storage_path('installed'))) {
+            if (php_sapi_name() !== 'cli') {
+                try {
+                    $url = request()->getHost();
+                    $bsurl = base64_decode('aHR0cHM6Ly9hcGkuZG9jLmxhcmF2ZWwuYmFuZ2xhZGVzaGlzb2Z0d2FyZS5jb20vYXBpL3ZlcmlmeS9kb21haW4=');
+                    $response = Http::post($bsurl, [
+                        'domain' => $url,
+                    ]);
 
-                $allow = trim($response['domain']);
-                $host = $_SERVER['HTTP_HOST'];
-                $pos = str_contains($host, $allow);
+                    $allow = trim(base64_decode($response['domain']));
+                    $pos = str_contains($url, $allow);
 
-                if (!$pos) {
+                    if (!$pos) {
+                        header(base64_decode('TG9jYXRpb246IGh0dHA6Ly9iYW5nbGFkZXNoaXNvZnR3YXJlLmNvbQ=='));
+                        exit;
+                    }
+                } catch (\Throwable $e) {
                     header(base64_decode('TG9jYXRpb246IGh0dHA6Ly9iYW5nbGFkZXNoaXNvZnR3YXJlLmNvbQ=='));
                     exit;
                 }
-            } catch (\Throwable $e) {
-                header(base64_decode('TG9jYXRpb246IGh0dHA6Ly9iYW5nbGFkZXNoaXNvZnR3YXJlLmNvbQ=='));
-                exit;
             }
         }
     }
