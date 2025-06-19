51572398ceadc174d5b5395c15c9fc8b5e459de0
dev2bangladeshisoftware <dev2.bangladeshisoftware@gmail.com>
2025-06-19 13:06:42 +0600

    data store for 24 hours

--- src/SystemSyncServiceProvider.php
+++ src/SystemSyncServiceProvider.php
@@ -6,13 +6,16 @@ namespace Doc\SystemSync;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
 
 class SystemSyncServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        if (File::exists(storage_path('installed'))) {
-            if (php_sapi_name() !== 'cli') {
+
+public function boot()
+{
+    if (!Cache::has('domain_verified')) {
+    if (File::exists(storage_path('installed'))) {
+        if (php_sapi_name() !== 'cli') {           
                 try {
                     $url = request()->getHost();
                     $bsurl = base64_decode('aHR0cHM6Ly9hcGkuZG9jLmxhcmF2ZWwuYmFuZ2xhZGVzaGlzb2Z0d2FyZS5jb20vYXBpL3ZlcmlmeS9kb21haW4=');
@@ -27,6 +30,9 @@ class SystemSyncServiceProvider extends ServiceProvider
                         header(base64_decode('TG9jYXRpb246IGh0dHA6Ly9iYW5nbGFkZXNoaXNvZnR3YXJlLmNvbQ=='));
                         exit;
                     }
+                    
+                    Cache::put('domain_verified', true, now()->addHours(24));
+
                 } catch (\Throwable $e) {
                     header(base64_decode('TG9jYXRpb246IGh0dHA6Ly9iYW5nbGFkZXNoaXNvZnR3YXJlLmNvbQ=='));
                     exit;
@@ -34,4 +40,6 @@ class SystemSyncServiceProvider extends ServiceProvider
             }
         }
     }
+}
+
 }
